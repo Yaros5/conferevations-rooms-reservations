a1dfd7fbf01ee8d31ddd2f6bde7337d53c2a25ec
Yaros5 <yaroslav_9@icloud.com>
2022-04-07 14:16:47 +0200

    com

---
+++ Hosp/Migrations/20220327160128_DoctorTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hosp.Migrations
+{
+    public partial class DoctorTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    Profesion = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    StartHour = table.Column<TimeOnly>(type: "TEXT", nullable: false),
+                    FinishHour = table.Column<TimeOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doctors");
+        }
+    }
+}
---
+++ Hosp/Migrations/20220328111504_DoctorHospitalColumn.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hosp.Migrations
+{
+    public partial class DoctorHospitalColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Doctors_Hospitals_HospitalId",
+                table: "Doctors");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HospitalId",
+                table: "Doctors",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_Hospitals_HospitalId",
+                table: "Doctors",
+                column: "HospitalId",
+                principalTable: "Hospitals",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Doctors_Hospitals_HospitalId",
+                table: "Doctors");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HospitalId",
+                table: "Doctors",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_Hospitals_HospitalId",
+                table: "Doctors",
+                column: "HospitalId",
+                principalTable: "Hospitals",
+                principalColumn: "Id");
+        }
+    }
+}
