e6dfb42e4e5c14c526e1e0d41a907894362d7249
Yaros5 <yaroslav_9@icloud.com>
2022-04-08 13:11:31 +0200

    com

---
+++ Hosp/Migrations/20220407134129_TimeOnlyToString.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hosp.Migrations
+{
+    public partial class TimeOnlyToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
